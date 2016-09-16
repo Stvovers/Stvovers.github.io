8a70c4c90e47e2133d5c460310d6383babe091d8
Stvovers <stijn_vanoverschelde@hotmail.com>
2016-09-16 10:20:43 +0200

    Add files via upload

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -1,12 +1,12 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/m/MessageToast"
-], function (Controller, MessageToast) {
+], function(Controller, MessageToast) {
 	"use strict";
 
 	return Controller.extend("sap.ui.demo.wt.controller.App", {
 
-		onShowHello : function () {
+		onShowHello: function() {
 			// read msg from i18n model
 			var oBundle = this.getView().getModel("i18n").getResourceBundle();
 			var sRecipient = this.getView().getModel().getProperty("/recipient/name");
@@ -15,33 +15,103 @@ sap.ui.define([
 			// show message
 			MessageToast.show(sMsg);
 		}
-		
+
 		,
-		
-		onSearchOTRS: function(oEvent){
-			
+
+		onSearchOTRS: function(oEvent) {
+
 			//alert("Search triggered: " + oEvent.getParameter("clearButtonPressed"));
-			
-			if 	(oEvent.getParameter("clearButtonPressed")== false){
+
+			if (oEvent.getParameter("clearButtonPressed") == false) {
 				var TicketNumber = oEvent.getParameter("query");
 				var URL = "http://otrs.msnet.railb.be/otrs/index.pl?Action=AgentTicketZoom;TicketNumber=" + TicketNumber;
 				window.open(URL, '_blank');
+
 			}
-		//alert("Search triggered: " + oEvent.getParameter("query"));
+			//alert("Search triggered: " + oEvent.getParameter("query"));
 		},
-		
-		onSearchEIM: function(oEvent){
-			
+
+		onSearchEIM: function(oEvent) {
+
 			//alert("Search triggered: " + oEvent.getParameter("clearButtonPressed"));
-			
-			if 	(oEvent.getParameter("clearButtonPressed")== false){
+
+			if (oEvent.getParameter("clearButtonPressed") == false) {
 				var EIM = oEvent.getParameter("query");
 				var URL = "http://eim.belgianrail.be/otcs01/llisapi.dll/open/FSP" + EIM;
 				window.open(URL, '_blank');
+
 			}
-		//alert("Search triggered: " + oEvent.getParameter("query"));
-		}
+			//alert("Search triggered: " + oEvent.getParameter("query"));
+		},
+		onPressCopyEIM: function(oEvent) {
+			var URL;
+			var EIM = this.getView().byId("EIM").getValue();
+			
+			/*MessageToast.show(sap.ui.Device.browser.name);*/
+			
+			if (EIM !== "") {
+
+				switch (sap.ui.Device.browser.name) {
+
+					case "cr":
+						URL = "http://eim.belgianrail.be/otcs01/llisapi.dll/open/FSP" + EIM;
+						var input = document.createElement('textarea');
+						document.body.appendChild(input);
+						input.value = (URL);
+						input.focus();
+						input.select();
+						document.execCommand('Copy');
+						input.remove();
+						MessageToast.show("EIM Link copied to clipboard!");
+						break;
+					case "ie":
+						URL = "http://eim.belgianrail.be/otcs01/llisapi.dll/open/FSP" + EIM;
+						/*MessageToast.show(source);*/
+						window.clipboardData.setData('text', URL);
+						MessageToast.show("EIM Link copied to clipboard!");
+
+				}
+			}
+
+		},
+		onPressCopyOTRS: function(oEvent) {
+			var URL;
+			var OTRS = this.getView().byId("OTRS").getValue();
+			
+			/*MessageToast.show(sap.ui.Device.browser.name);*/
+			
+			if (OTRS !== "") {
+
+				switch (sap.ui.Device.browser.name) {
+
+					case "cr":
+						URL = "http://otrs.msnet.railb.be/otrs/index.pl?Action=AgentTicketZoom;TicketNumber=" + OTRS;
+						var input = document.createElement('textarea');
+						document.body.appendChild(input);
+						input.value = (URL);
+						input.focus();
+						input.select();
+						document.execCommand('Copy');
+						input.remove();
+						MessageToast.show("EIM Link copied to clipboard!");
+						break;
+					case "ie":
+						URL = "http://otrs.msnet.railb.be/otrs/index.pl?Action=AgentTicketZoom;TicketNumber=" + OTRS;
+						/*MessageToast.show(source);*/
+						window.clipboardData.setData('text', URL);
+						MessageToast.show("EIM Link copied to clipboard!");
+
+				}
+			}
+
+		},
 		
+		onPressTileOTRSOwner: function(oEvent){
+				var URL = "http://otrs.msnet.railb.be/otrs/index.pl?Action=AgentTicketSearch;Subaction=Search;TakeLastSearch=1;SaveProfile=1;Profile=My%20Tickets%20%28Owner%29";
+				window.open(URL, '_blank');
+			
+		}
+
 	});
 
 });
