09c1263415920ff73e981dac7b80f4aabd4fcf58
Stvovers <stijn_vanoverschelde@hotmail.com>
2016-11-29 14:40:54 +0000

    Add Charm

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -43,6 +43,18 @@ sap.ui.define([
 			}
 			//alert("Search triggered: " + oEvent.getParameter("query"));
 		},
+		onSearchCharm: function(oEvent) {
+
+			//alert("Search triggered: " + oEvent.getParameter("clearButtonPressed"));
+
+			if (oEvent.getParameter("clearButtonPressed") == false) {
+				var Charm = oEvent.getParameter("query");
+				var URL = "http://sapxbp.msnet.railb.be:8102/sap/bc/bsp/sap/crm_ui_start/default.htm?crm-object-type=AIC_OB_CMCD&crm-object-action=B&crm-object-keyname=OBJECT_ID&crm-object-value=" + Charm;
+				window.open(URL, '_blank');
+
+			}
+			//alert("Search triggered: " + oEvent.getParameter("query"));
+		},
 		onPressCopyEIM: function(oEvent) {
 			var URL;
 			var EIM = this.getView().byId("EIM").getValue();
@@ -105,7 +117,37 @@ sap.ui.define([
 			}
 
 		},
+			onPressCopyCharm: function(oEvent) {
+			var URL;
+			var Charm = this.getView().byId("Charm").getValue();
 
+			/*MessageToast.show(sap.ui.Device.browser.name);*/
+
+			if (Charm !== "") {
+
+				switch (sap.ui.Device.browser.name) {
+
+					case "cr":
+						URL = "http://sapxbp.msnet.railb.be:8102/sap/bc/bsp/sap/crm_ui_start/default.htm?crm-object-type=AIC_OB_CMCD&crm-object-action=B&crm-object-keyname=OBJECT_ID&crm-object-value=" + Charm;
+						var input = document.createElement('textarea');
+						document.body.appendChild(input);
+						input.value = (URL);
+						input.focus();
+						input.select();
+						document.execCommand('Copy');
+						input.remove();
+						MessageToast.show("Charm Link copied to clipboard!");
+						break;
+					case "ie":
+						URL = "http://sapxbp.msnet.railb.be:8102/sap/bc/bsp/sap/crm_ui_start/default.htm?crm-object-type=AIC_OB_CMCD&crm-object-action=B&crm-object-keyname=OBJECT_ID&crm-object-value=" + Charm;
+						/*MessageToast.show(source);*/
+						window.clipboardData.setData('text', URL);
+						MessageToast.show("Charm Link copied to clipboard!");
+
+				}
+			}
+
+		},
 		onPressTileOTRSOwner: function(oEvent) {
 			var URL =
 				"http://otrs.msnet.railb.be/otrs/index.pl?Action=AgentTicketSearch;Subaction=Search;TakeLastSearch=1;SaveProfile=1;Profile=My%20Tickets%20%28Owner%29";
